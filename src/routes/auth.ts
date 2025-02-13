8e4ae86c05e9d27f62389761b3c33bc2aa705fe2
Muhammad Saqib <muhammadsackib@gmail.com>
2025-02-13 21:02:28 +0500

    register endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,31 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import { Prisma } from "@prisma/client";
+
+import { prisma } from "../lib/prisma";
+import { User } from "../schemas";
+import { ZodError } from "zod";
+import { SALT_ROUNDS } from "../config/constants";
 
 export const authRouter = express.Router();
 
-authRouter.post("/register", (req, res) => {
-  res.json(req.body);
+authRouter.post("/register", async (req, res) => {
+  try {
+    const user = User.parse(req.body);
+    const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+    await prisma.user.create({ data: { ...user, password: hash } });
+    res.sendStatus(201);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(400).json(error.issues);
+    }
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        res
+          .status(400)
+          .json({ error: "User with this email/username already exists" });
+      }
+    }
+  }
 });
