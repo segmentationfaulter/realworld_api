1f4e449e4fef39a9740141c3966aefc35b14231d
Muhammad Saqib <muhammadsackib@gmail.com>
2025-02-15 11:43:57 +0500

    add experss-jwt

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import express from "express";
+import { expressjwt as jwt } from "express-jwt";
 import { authRouter } from "./routes/auth.ts";
 
 const app = express();
 
 app.use(express.json());
 app.use(authRouter);
+app.use(
+  jwt({ secret: process.env.JWT_SECRET as string, algorithms: ["HS256"] }),
+);
 
 app.listen(process.env.PORT, () => {
   console.info(`Server listening on port ${process.env.PORT}`);
